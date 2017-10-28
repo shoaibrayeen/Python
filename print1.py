84a7e4dcec54c368b1f97d7ef3b1b91252c492bf
MOHD SHOAIB RAYEEN <31645414+shoaibrayeen@users.noreply.github.com>
2017-10-28 17:56:49 +0530

    Print a Triangle and A Square by functions

---
+++ print1.py
@@ -0,0 +1,24 @@
+def triangle():
+    # To print a triangle
+    print('   *')
+    print('  ***')
+    print(' *****')
+    print('*******')
+    
+def square():
+    # To print a square
+    print('* * * *')
+    print('* * * *')
+    print('* * * *')
+    print('* * * *')
+    
+def main():
+    # To print a triangle
+    triangle()
+    print()
+    # To print a square
+    square()
+    
+if __name__=='__main__':
+    main()
+print('\nEnd of program')
