373d8ef2e2f4430472e21dea892194ebde6dff44
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-11-25 21:51:20 +0530

    Type of different type of variables

---
+++ Quiz/type_of_variables.py
@@ -0,0 +1,29 @@
+a=(2) #int
+b=(2.5)#float
+c=(2,)#tuple
+d=1#int
+e=1.5#float
+f=[1,] #list of same data type
+g=[1,2,3.0,'3',"12345","abc"] #list of different data type
+h=([1,2],4,5.0,'5',"abc")    #tuple of different data type
+i=("abc") #str
+j=('abc') #str
+k=('a')   #str
+l=("a")   #str
+m='a'     #str
+
+print("\n\nType of a\t:\t",type(a))
+print("\n\nType of b\t:\t",type(b))
+print("\n\nType of c\t:\t",type(c))
+print("\n\nType of d\t:\t",type(d))
+print("\n\nType of e\t:\t",type(e))
+print("\n\nType of f\t:\t",type(f))
+print("\n\nType of g\t:\t",type(g))
+print("\n\nType of h\t:\t",type(h))
+print("\n\nType of i\t:\t",type(i))
+print("\n\nType of j\t:\t",type(j))
+print("\n\nType of k\t:\t",type(k))
+print("\n\nType of l\t:\t",type(l))
+print("\n\nType of m\t:\t",type(m))
+
+
