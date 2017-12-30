42f5e132799e947e0580550447eab9f32cdbd519
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-12-31 02:07:52 +0530

    Basic Recursion Problems

---
+++ Problems/Recursion/increment.py
@@ -0,0 +1,14 @@
+def increment(number):
+    '''
+    objective: To increment number by 1
+    input values:
+        number: The number to be incremented
+
+    output value: The Incremented number
+    '''
+    #approach: Add the number with 1
+    return number + 1
+
+
+number = int(input('Enter number : '))
+print ( 'Incremented Number : ' , increment(number) )
