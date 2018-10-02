ffadca34fce89f4be3a9f07bec1dc9c50cd8c3aa
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-10-02 22:03:28 +0530

    insert its proper position into a sorted list

--- List/Insert.py
+++ List/Insert.py
@@ -6,10 +6,10 @@ Input parameters :
                n : element to be inserted
 output           : sorted list
 '''
-if l==[]:
- return l.insert(0,n)
-if l[0]>n:
- return l.insert(0,n)
-else
- SortList(l[1:],n)
+  if l==[]:
+    return l.insert(0,n)
+  if l[0]>n:
+    return l.insert(0,n)
+  else:
+    SortList(l[1:],n)
  
